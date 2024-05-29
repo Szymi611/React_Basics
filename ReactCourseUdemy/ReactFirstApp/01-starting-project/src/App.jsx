2c5869db9960da6c66bb94f9a1ae5c478fa0bea3
Szymi611 <szymondomagala32@gmail.com>
2024-05-29 20:37:03 +0200

    Learning how react works basics planing of the project

--- ReactCourseUdemy/ReactFirstApp/01-starting-project/src/App.jsx
+++ ReactCourseUdemy/ReactFirstApp/01-starting-project/src/App.jsx
@@ -1,30 +1,26 @@
 import reactImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png';
+import { CORE_CONCEPTS } from './data.js';
+import Header from './components/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
 
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max){
-  return Math.floor(Math.random() * (max+1));
-}
-
-function Header(){
-  return(
-    <header>
-    <img src={reactImg} alt="Stylized atom" />
-    <h1>React Essentials</h1>
-    <p>
-      {reactDescriptions[genRandomInt(2)]} React concepts you will need for almost any app you are
-      going to build!
-    </p>
-    </header>
-  );
-}
 
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id='core-concepts'>
+          <h2>Time to get started!</h2>
+          <ul>
+            <CoreConcept title="Component" description="The core UI building block" image={componentsImg}/>
+            <CoreConcept {...CORE_CONCEPTS[0]}/>
+            <CoreConcept title={CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description} image={CORE_CONCEPTS[1].image}/>
+            <CoreConcept title={CORE_CONCEPTS[2].title} description={CORE_CONCEPTS[2].description} image={CORE_CONCEPTS[2].image}/>
+            <CoreConcept title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} image={CORE_CONCEPTS[3].image}/>
+            <CoreConcept title="Component" description="The core UI building block" image={componentsImg}/>
+          </ul>
+        </section>
       </main>
     </div>
   );
