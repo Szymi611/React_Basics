1056f1596dc94b4f4addaaa70fd2a737ee979d77
Szymi611 <szymondomagala32@gmail.com>
2024-10-04 14:15:35 +0200

    Learning about useState and components on tic tac toe game project

--- ReactCourseUdemy/ReactFirstApp/07-tic-tac-toe-starting-project/src/App.jsx
+++ ReactCourseUdemy/ReactFirstApp/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,10 +1,80 @@
+import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
+import Log from "./components/Log";
+import GameOver from "./components/GameOver.jsx";
+import { WINNING_COMBINATIONS} from './winning-combinations.js'
+import { useState } from "react";
+
+const initialGameBoard =[
+  [null, null, null], 
+  [null, null, null], 
+  [null, null, null],
+];
+
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = 'X';
+
+  if(gameTurns.length > 0 && gameTurns[0].player === 'X'){
+    currentPlayer = 'O';
+  }
+  return currentPlayer;
+}
 
 function App() {
-  
+  const [gameTurns, setGameTurns] = useState([]);
+  // const [activePlayer, setActivePlayer] = useState("X");
+
+  const activePlayer = deriveActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+
+  for (const turn of gameTurns) {
+      const { square, player } = turn;
+      const { row, col } = square;
+
+      gameBoard[row][col] = player;
+  }
+
+  let winner;
+
+  for (const combinations of WINNING_COMBINATIONS){
+    const firstSquareSymbol = gameBoard[combinations[0].row][combinations[0].column];  
+    const secondSquareSymbol = gameBoard[combinations[1].row][combinations[1].column];  
+    const thirdSquareSymbol = gameBoard[combinations[2].row][combinations[2].column];  
+
+    if(firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol){
+      winner = firstSquareSymbol
+    }
+  }
+
+  const hasDraw = gameTurns.length === 9 && !winner;
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    //setActivePlayer((currActivePlayer)=> currActivePlayer === "X" ? "O" : "X");
+    
+    setGameTurns((prevTurns) => {
+      const currentPlayer = deriveActivePlayer(prevTurns);
+
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex}, player: currentPlayer },
+         ...prevTurns];
+         return updatedTurns;
+    });
+  }
 
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          <Player initialName='Player1' symbol='X' isActive={activePlayer === 'X'}/>
+          <Player initialName='Player2' symbol='O' isActive={activePlayer === 'O'}/>
+        </ol>
+        {(winner || hasDraw) && <GameOver winner={winner}/>}
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard}/>
+      </div>
+      <Log turns={gameTurns}/>
+    </main>
+    );
 }
 
 export default App
