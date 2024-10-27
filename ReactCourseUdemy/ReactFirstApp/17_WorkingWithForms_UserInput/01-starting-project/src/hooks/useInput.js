e1076b0fed1498cf64ac3b06afa33867e914c060
Szymi611 <szymondomagala32@gmail.com>
2024-10-27 12:17:51 +0100

    Using custom useInput hook

---
+++ ReactCourseUdemy/ReactFirstApp/17_WorkingWithForms_UserInput/01-starting-project/src/hooks/useInput.js
@@ -0,0 +1,25 @@
+import {useState} from 'react';
+
+export function useInput(defualtValue, validationFn) {
+  const [enteredValue, setEnteredValue] = useState(defualtValue);
+
+  const [didEdit, setDidEdit] = useState(false);
+
+  const valueIsValid = validationFn(enteredValue)
+
+  function handleInputChange(event) {
+    setEnteredValue(event.target.value);
+    setDidEdit(false);
+  }
+
+  function handleInputBlur(identifier) {
+    setDidEdit(true);
+  }
+
+  return {
+    value: enteredValue,
+    handleInputChange,
+    handleInputBlur,
+    hasError: didEdit && !valueIsValid
+  }
+}
