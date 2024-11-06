5a5b4e7a9e15a48f58e65bdd0bb5649973c8cd95
Szymi611 <szymondomagala32@gmail.com>
2024-11-06 19:40:07 +0100

    Using useReducer to adding meal items to the cart

--- ReactCourseUdemy/ReactFirstApp/18_FoodOrderApp/01-starting-project/src/store/CardContext.jsx
+++ ReactCourseUdemy/ReactFirstApp/18_FoodOrderApp/01-starting-project/src/store/CardContext.jsx
@@ -1,7 +1,42 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 const CardContext = createContext({
   items: [],
   addItem: (item) => {},
   removeItem: (id) => {},
 });
+
+function cartReducer(state, action) {
+  if (action.type === "ADD_IITEM") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    const upadtedItems = [...state.items]; //Copy of the old array
+
+    if (existingCartItemIndex > -1) {
+      const existingItem = state.items[existingCartItemIndex];
+      const updatedItem = {
+        ...existingItem,
+        quantity: existingItem.quantity + 1,
+      };
+      upadtedItems[existingCartItemIndex] = updatedItem;
+    }else{
+      upadtedItems.push({...action.item, quantity: 1});
+    }
+    return { ...state, items: upadtedItems};
+  }
+  if (action.type === "REMOVE_ITEM") {
+    // ... update the state to remove a meal item
+  }
+
+  return state;
+}
+
+export function CardContextProvider({ children }) {
+  useReducer(cartReducer, { items: [] });
+
+  return <CardContext.Provider>{children}</CardContext.Provider>;
+}
+
+export default CardContext;
