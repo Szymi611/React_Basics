89686c723cb20a921621106503f3d1fbf523bbb8
Szymi611 <szymondomagala32@gmail.com>
2024-11-06 20:20:04 +0100

    Finishing cartContext( adding and removing items from the cart and dynamically displaying amount of items in the cart)

--- ReactCourseUdemy/ReactFirstApp/18_FoodOrderApp/01-starting-project/src/components/MealItem.jsx
+++ ReactCourseUdemy/ReactFirstApp/18_FoodOrderApp/01-starting-project/src/components/MealItem.jsx
@@ -1,7 +1,15 @@
+import { useContext } from "react";
 import { currencyFormatter } from "../util/formatting";
 import Button from "./UI/Button";
+import CardContext from "../store/CartContext.jsx";
 
 export default function MealItem({ meal }) {
+  const cartCtx = useContext(CardContext);
+
+  function handleAddMealToCart() {
+    cartCtx.addItem(meal);
+  }
+  
   return (
     <li className="meal-item">
       <article>
@@ -12,7 +20,7 @@ export default function MealItem({ meal }) {
           <p className="meal-item-description">{meal.description}</p>
         </div>
         <p className="meal-item-actions">
-          <Button>Add to Cart</Button>
+          <Button onClick={handleAddMealToCart}>Add to Cart</Button>
         </p>
       </article>
     </li>
