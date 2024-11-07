c971533d8c0997a0d44d6c7c9857bd6039e32121
Szymi611 <szymondomagala32@gmail.com>
2024-11-07 19:13:31 +0100

    Creating a modal for cart

--- ReactCourseUdemy/ReactFirstApp/18_FoodOrderApp/01-starting-project/src/components/Header.jsx
+++ ReactCourseUdemy/ReactFirstApp/18_FoodOrderApp/01-starting-project/src/components/Header.jsx
@@ -1,23 +1,33 @@
+
+import { useContext } from 'react';
+
+import Button from './UI/Button.jsx';
 import logoImg from '../assets/logo.jpg';
 import CartContext from '../store/CartContext.jsx';
-import Button from './UI/Button.jsx';
-import { useContext } from 'react';
+import UserProgressContext from '../store/UserProgressContext.jsx';
 
 export default function Header() {
   const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
 
   const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
     return totalNumberOfItems + item.quantity;
   }, 0);
 
-  return(
+  function handleShowCart() {
+    userProgressCtx.showCart();
+  }
+
+  return (
     <header id="main-header">
       <div id="title">
-        <img src={logoImg} alt="A restaurant logo" />
+        <img src={logoImg} alt="A restaurant" />
         <h1>ReactFood</h1>
       </div>
       <nav>
-        <Button textOnly>Cart ({totalCartItems})</Button>
+        <Button textOnly onClick={handleShowCart}>
+          Cart ({totalCartItems})
+        </Button>
       </nav>
     </header>
   );
