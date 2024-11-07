6c6dba4f6a1b6723aaff1dc496e9f8b21977efda
Szymi611 <szymondomagala32@gmail.com>
2024-11-07 19:35:08 +0100

    Creating cart items with some styles and working logic

--- ReactCourseUdemy/ReactFirstApp/18_FoodOrderApp/01-starting-project/src/components/Cart.jsx
+++ ReactCourseUdemy/ReactFirstApp/18_FoodOrderApp/01-starting-project/src/components/Cart.jsx
@@ -1,36 +1,45 @@
-import Modal from "./UI/Modal";
-import CartContext from "../store/CartContext";
-import Button from "./UI/Button";
-import { useContext } from "react";
-import { currencyFormatter } from "../util/formatting";
-import UserProgressContext from "../store/UserProgressContext.jsx";
+import { useContext } from 'react';
+
+import Modal from './UI/Modal.jsx';
+import CartContext from '../store/CartContext.jsx';
+import Button from './UI/Button.jsx';
+import { currencyFormatter } from '../util/formatting.js';
+import UserProgressContext from '../store/UserProgressContext.jsx';
+import CartItem from './CartItem.jsx';
 
 export default function Cart() {
   const cartCtx = useContext(CartContext);
-  const userProgressCtx = useContext(UserProgressContext)
+  const userProgressCtx = useContext(UserProgressContext);
 
   const cartTotal = cartCtx.items.reduce(
     (totalPrice, item) => totalPrice + item.quantity * item.price,
     0
   );
 
-  function handleCloseCart(){
+  function handleCloseCart() {
     userProgressCtx.hideCart();
   }
 
   return (
     <Modal className="cart" open={userProgressCtx.progress === 'cart'}>
-      <h2>Your cart</h2>
+      <h2>Your Cart</h2>
       <ul>
         {cartCtx.items.map((item) => (
-          <li key={item.id}>
-            {item.name} - {item.quantity}
-          </li>
+          <CartItem
+            key={item.id}
+            name={item.name}
+            quantity={item.quantity}
+            price={item.price}
+            onIncrease={() => cartCtx.addItem(item)}
+            onDecrease={() => cartCtx.removeItem(item.id)}
+          />
         ))}
       </ul>
       <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
       <p className="modal-actions">
-        <Button textOnly onClick={handleCloseCart}>Close</Button>
+        <Button textOnly onClick={handleCloseCart}>
+          Close
+        </Button>
         <Button onClick={handleCloseCart}>Go to Checkout</Button>
       </p>
     </Modal>
