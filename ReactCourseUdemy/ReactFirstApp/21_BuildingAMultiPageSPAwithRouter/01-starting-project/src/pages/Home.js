a54dd8a85a8e3a28f1e4eee7f9b8b8cc34037870
Szymi611 <szymondomagala32@gmail.com>
2024-11-12 18:06:09 +0100

    Link component

--- ReactCourseUdemy/ReactFirstApp/21_BuildingAMultiPageSPAwithRouter/01-starting-project/src/pages/Home.js
+++ ReactCourseUdemy/ReactFirstApp/21_BuildingAMultiPageSPAwithRouter/01-starting-project/src/pages/Home.js
@@ -1,6 +1,12 @@
+import {Link} from 'react-router-dom'
+
 function HomePage(){
   return(
-    <h1>My main page</h1>
+    <>
+      <h1>My main page</h1>
+      <p>Go to <Link to="/products"> the list of products</Link></p>
+    </>
+
   )
 }
 
