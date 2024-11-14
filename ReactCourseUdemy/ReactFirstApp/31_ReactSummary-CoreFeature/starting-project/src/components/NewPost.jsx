c78d5f8a56e35867f3dd39b2358a775829ec8995
Szymi611 <szymondomagala32@gmail.com>
2024-11-14 19:05:00 +0100

    Using state to get user input

--- ReactCourseUdemy/ReactFirstApp/31_ReactSummary-CoreFeature/starting-project/src/components/NewPost.jsx
+++ ReactCourseUdemy/ReactFirstApp/31_ReactSummary-CoreFeature/starting-project/src/components/NewPost.jsx
@@ -1,12 +1,20 @@
+import { useState } from 'react';
 import styles from './NewPost.module.css'
 
 export default function NewPost(){
+  const [enteredBody, setEnteredBody] = useState('');
+
+  function bodyChangeHandler(event){
+    setEnteredBody(event.target.value);
+  }
+
   return(
     <form className={styles.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea name="body" id="body" required rows='3'/>
+        <textarea name="body" id="body" required rows='3' onChange={bodyChangeHandler}/>
       </p>
+      <p>{enteredBody}</p>
       <p>
         <label htmlFor="author">Author</label>
         <input type="text" name="author" id="author" required/>
