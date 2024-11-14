555d36d7bbcf873cedb43792be9f778a94c461d3
Szymi611 <szymondomagala32@gmail.com>
2024-11-14 19:38:14 +0100

    Lifiting up the state

--- ReactCourseUdemy/ReactFirstApp/31_ReactSummary-CoreFeature/starting-project/src/components/NewPost.jsx
+++ ReactCourseUdemy/ReactFirstApp/31_ReactSummary-CoreFeature/starting-project/src/components/NewPost.jsx
@@ -1,24 +1,20 @@
 import { useState } from 'react';
 import styles from './NewPost.module.css'
 
-export default function NewPost(){
-  const [enteredBody, setEnteredBody] = useState('');
-
-  function bodyChangeHandler(event){
-    setEnteredBody(event.target.value);
-  }
+export default function NewPost({onAuthorChange, onBodyChange, body, author}){
 
   return(
     <form className={styles.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea name="body" id="body" required rows='3' onChange={bodyChangeHandler}/>
+        <textarea name="body" id="body" required rows='3' onChange={onBodyChange}/>
       </p>
-      <p>{enteredBody}</p>
+      <p>{body}</p>
       <p>
         <label htmlFor="author">Author</label>
-        <input type="text" name="author" id="author" required/>
+        <input type="text" name="author" id="author" required onChange={onAuthorChange}/>
       </p>
+      <p>{author}</p>
     </form>
   );
 } 
--- ReactCourseUdemy/ReactFirstApp/31_ReactSummary-CoreFeature/starting-project/src/components/PostsList.jsx
+++ ReactCourseUdemy/ReactFirstApp/31_ReactSummary-CoreFeature/starting-project/src/components/PostsList.jsx
@@ -1,12 +1,24 @@
 import NewPost from "./NewPost";
 import Post from "./Post";
 import styles from "./PostsList.module.css";
+import { useState } from "react";
+
 export default function PostsList() {
+  const [enteredBody, setEnteredBody] = useState('');
+  const [enteredAuthor, setEnteredAuthor] = useState('');
+  function bodyChangeHandler(event){
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event){
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
     <>
-      <NewPost />
+      <NewPost onBodyChange={bodyChangeHandler} body={enteredBody} author={enteredAuthor} onAuthorChange={authorChangeHandler}/>
       <ul className={styles.posts}>
-        <Post name={"Max"} message={"Hello World"} />
+        <Post name={enteredAuthor} message={enteredBody} />
         <Post name={"Wojtek"} message={"Hola amigo!"} />
       </ul>
     </>
