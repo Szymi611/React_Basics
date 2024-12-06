40e2019a44a5a33444d58855829ca8988c291b6a
Szymi611 <szymondomagala32@gmail.com>
2024-12-06 15:15:51 +0100

    Learing about primitives

---
+++ ReactCourseUdemy/ReactFirstApp/30_React-TypeScript/01-starting-project/basics.ts
@@ -0,0 +1,12 @@
+// Primitives: number, string, boolean
+// More complex types: arrays, objects
+// Function types, parameters
+
+// Primitives
+let age:number = 30;
+let username:string = 'Wojtek';
+let isInstructor:boolean = true;
+
+let hobiess: null;
+
+// hobiess = 1; Error bc of type delcared upper
