3530bb223aef11c2d92faa9fb658d9d3da6808fa
Szymi611 <szymondomagala32@gmail.com>
2024-12-06 15:20:58 +0100

    More complex types

--- ReactCourseUdemy/ReactFirstApp/30_React-TypeScript/01-starting-project/basics.ts
+++ ReactCourseUdemy/ReactFirstApp/30_React-TypeScript/01-starting-project/basics.ts
@@ -3,10 +3,34 @@
 // Function types, parameters
 
 // Primitives
-let age:number = 30;
-let username:string = 'Wojtek';
-let isInstructor:boolean = true;
+let age: number = 30;
+let username: string = "Wojtek";
+let isInstructor: boolean = true;
 
 let hobiess: null;
 
 // hobiess = 1; Error bc of type delcared upper
+
+// More complex types
+
+let hobbies: string[];
+hobbies = ["Sports", "Cooking"];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Wojtek",
+  age: 30,
+};
+
+// person = {
+//   isEmployee: true,
+// }; 
+
+let people :{
+  name: string;
+  age: number;
+}[]; // array of objects
