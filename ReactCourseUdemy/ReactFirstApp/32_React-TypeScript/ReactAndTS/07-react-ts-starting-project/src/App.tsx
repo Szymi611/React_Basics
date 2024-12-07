935db0226c1350d9b98ad5ccbd0c59b97f6c67c4
Szymi611 <szymondomagala32@gmail.com>
2024-12-07 18:59:00 +0100

    Adding basic list to the app

--- ReactCourseUdemy/ReactFirstApp/32_React-TypeScript/ReactAndTS/07-react-ts-starting-project/src/App.tsx
+++ ReactCourseUdemy/ReactFirstApp/32_React-TypeScript/ReactAndTS/07-react-ts-starting-project/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import Todos from './components/Todos.tsx';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Todos />
     </div>
   );
 }
