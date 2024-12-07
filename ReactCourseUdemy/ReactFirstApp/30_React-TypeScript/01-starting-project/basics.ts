c1e73c07d88c4a36a0f4059804670533f673d48e
Szymi611 <szymondomagala32@gmail.com>
2024-12-07 18:40:45 +0100

    Generics

--- ReactCourseUdemy/ReactFirstApp/30_React-TypeScript/01-starting-project/basics.ts
+++ ReactCourseUdemy/ReactFirstApp/30_React-TypeScript/01-starting-project/basics.ts
@@ -44,3 +44,25 @@ let people2: Person[]; // array of objects
 let course: string | number = "React - The Complete Guide";
 
 course = 1234; 
+
+// Functions & types
+
+function add(a: number,b: number) : number {
+  return a + b;
+}
+
+function print(value: any) {
+  console.log(value);
+}
+
+// Generics
+
+function insertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array]
+  return newArray;
+}
+
+const demoArray = [1,2,3];
+
+const upadatedArray = insertAtBeginning(demoArray, 0); // [0,1,2,3]
+const stringArray = insertAtBeginning(["a","b","c"],"d"); // ["d","a","b","c"]
