1e6cd9151b359f77b5d7d618b1958e5cdc56f57d
Szymi611 <szymondomagala32@gmail.com>
2024-12-08 19:50:26 +0100

    Adding a Data Model

--- ReactCourseUdemy/ReactFirstApp/32_React-TypeScript/ReactAndTS/07-react-ts-starting-project/src/App.tsx
+++ ReactCourseUdemy/ReactFirstApp/32_React-TypeScript/ReactAndTS/07-react-ts-starting-project/src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import Todos from './components/Todos.tsx';
+import Todo from './models/todo.ts';
 import './App.css';
 
 function App() {
+  const todos = [
+    new Todo('Learn React'),
+    new Todo('Learn Typescript')
+  ]
+
+  
   return (
     <div className="App">
-      <Todos items={['Learn React', 'Learn Typescript']}/>
+      <Todos items={todos}/>
     </div>
   );
 }
